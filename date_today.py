cdab13f3b75200d8b6cd07a2547927e32b078759
rlysak01 <richard.lysakowski@bcbsma.com>
2019-03-18 14:33:35 -0400

    add date_today.py

---
+++ date_today.py
@@ -0,0 +1,14 @@
+# this file will have a basic function
+
+import datetime
+
+def print_time()
+    print(f'Today is ({datetime.datetime.now()}')
+
+def main():
+    print_time()
+
+# runs program like a script
+if __name__ == "__main__":
+    main()
+
